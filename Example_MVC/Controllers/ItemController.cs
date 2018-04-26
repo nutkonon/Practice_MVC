0ba0c94406acb1bca3f4527b3addc5d573c9a183
Jennarong Srisuwan <nut_konon@hotmail.com>
2018-04-26 16:51:42 +0700

    Add project files.

---
+++ Example_MVC/Controllers/ItemController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Example_MVC.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Index()
+        {
+            ViewBag.ItemList = "Nutkonon Shop Item List Page";
+            return View();
+        }
+    }
+}
