67156c4971389f34db1a67fbd45b6f8844a64d44
Jennarong Srisuwan <nut_konon@hotmail.com>
2018-04-28 23:57:56 +0700

    Practice ViewBag,ViewData,TempData

--- Example_MVC/Controllers/HomeController.cs
+++ Example_MVC/Controllers/HomeController.cs
@@ -10,20 +10,28 @@ namespace Example_MVC.Controllers
     {
         public ActionResult Index()
         {
+            TempData["FirstValue"] = "Hello World";
             return View();
         }
 
         public ActionResult About()
         {
+            if (TempData["FirstValue"] != null)
+            {
+                TempData.Keep();
+                return RedirectToAction("Contact");
+            }
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
+            if (TempData["FirstValue"] != null)
+            {
+                ViewBag.MSG1 = TempData["FirstValue"].ToString();
+            }
             return View();
         }
 
