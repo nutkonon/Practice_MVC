d4085bc4ed143838ba685993f10ef07f88f99065
Jennarong Srisuwan <nut_konon@hotmail.com>
2018-04-29 17:52:07 +0700

    Practice Insert, Update, Delete without Entity Framework

--- Example_MVC/Controllers/ItemController.cs
+++ Example_MVC/Controllers/ItemController.cs
@@ -3,41 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Example_MVC.Models;
 
 namespace Example_MVC.Controllers
 {
     public class ItemController : Controller
     {
-        //GET: Item
+        // 1. *********** Display All Item List in Index Page ***********
         public ActionResult Index()
         {
             ViewBag.ItemList = "Nutkonon Shop Item List Page";
+            ItemDBHandler IHandler = new ItemDBHandler();
+            ModelState.Clear();
+            return View(IHandler.GetItemList());
+        }
+        // 2. *********** Add New Item ***********
+        [HttpGet]
+        public ActionResult Create()
+        {
             return View();
-
+        }
+        [HttpPost]
+        public ActionResult Create(ItemModel iList)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    ItemDBHandler ItemHandler = new ItemDBHandler();
+                    if (ItemHandler.InsertItem(iList))
+                    {
+                        ViewBag.Message = "Item Added Successfully";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch { return View(); }
         }
 
-        //public EmptyResult Index()
-        //{
-        //    ViewBag.ItemList = "Nutkonon Shop Item List Page";
-        //    return new EmptyResult();
-        //}
-
-        //public JsonResult Index()
-        //{
-        //    Itemlist list = new Itemlist()
-        //    {
-        //        ID = "id1",
-        //        Name = "Nutkonon",
-        //        Mobile = "0999999999"
-        //    };
-        //    return Json(emp, JsonRequestBehavior.AllowGet);
-        //}
-
-        public class Itemlist
+        // 3. *********** Update Item Details ***********
+        [HttpGet]
+        public ActionResult Edit(int id)
         {
-            public string ID { get; set; }
-            public string Name { get; set; }
-            public string Mobile { get; set; }
+            ItemDBHandler ItemHandler = new ItemDBHandler();
+            return View(ItemHandler.GetItemList().Find(itemmodel => itemmodel.ID == id));
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, ItemModel iList)
+        {
+            try
+            {
+                ItemDBHandler ItemHandler = new ItemDBHandler();
+                ItemHandler.UpdateItem(iList);
+                return RedirectToAction("Index");
+            }
+            catch { return View(); }
+        }
+
+        // 4. *********** Delete Item Details ***********
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            ItemDBHandler ItemHandler = new ItemDBHandler();
+            return View(ItemHandler.GetItemList().Find(itemmodel => itemmodel.ID == id));
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeletePost(int id)
+        {
+            try
+            {
+                ItemDBHandler ItemHandler = new ItemDBHandler();
+                if (ItemHandler.DeleteItem(id))
+                {
+                    ViewBag.AlertMsg = "Item Deleted Successfully";
+                }
+                return RedirectToAction("Index");
+            }
+            catch { return View(); }
+        }
+
+        public ActionResult Details(int id)
+        {
+            ItemDBHandler ItemHandler = new ItemDBHandler();
+            return View(ItemHandler.GetItemList().Find(itemmodel => itemmodel.ID == id));
         }
 
     }
---
+++ Example_MVC/Models/ItemDBHandler.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Example_MVC.Models
+{
+    public class ItemDBHandler
+    {
+        private SqlConnection con;
+        private void connection()
+        {
+            string constring = ConfigurationManager.ConnectionStrings["NutkononShop"].ToString();
+            con = new SqlConnection(constring);
+        }
+
+        // 1. ********** Insert Item **********
+        public bool InsertItem(ItemModel iList)
+        {
+            connection();
+            string query = "INSERT INTO ItemList VALUES('" + iList.Name + "','" + iList.Category + "'," + iList.Price + ")";
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+
+        // 2. ********** Get All Item List **********
+        public List<ItemModel> GetItemList()
+        {
+            connection();
+            List<ItemModel> iList = new List<ItemModel>();
+
+            string query = "SELECT * FROM ItemList";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            adapter.Fill(dt);
+            con.Close();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                iList.Add(new ItemModel
+                {
+                    ID = Convert.ToInt32(dr["ID"]),
+                    Name = Convert.ToString(dr["Name"]),
+                    Category = Convert.ToString(dr["Category"]),
+                    Price = Convert.ToDecimal(dr["Price"])
+                });
+            }
+            return iList;
+        }
+
+        // 3. ********** Update Item Details **********
+        public bool UpdateItem(ItemModel iList)
+        {
+            connection();
+            string query = "UPDATE ItemList SET Name = '" + iList.Name + "', Category = '" + iList.Category + "', Price = " + iList.Price + " WHERE ID = " + iList.ID;
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+
+        // 4. ********** Delete Item **********
+        public bool DeleteItem(int id)
+        {
+            connection();
+            string query = "DELETE FROM ItemList WHERE ID = " + id;
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
